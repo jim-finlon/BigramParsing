2022f456f89bd451391f23df98ef0981f7ea6e90
James D. Finlon <jim@finlon.net>
2016-12-12 11:24:03 -0700

    Update Program.cs

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 
@@ -9,78 +10,106 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
-            var stringToTest = "";
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+
             if (args.Any())
             {
                 var path = args[0];
+
                 if (File.Exists(path))
                 {
-                    TextReader input = File.OpenText(path);
-                    stringToTest = input.ReadToEnd();
+                    var service = new HistogramService();
+                    try
+                    {
+                        var results = service.ProcessFile(path);
+
+                        Console.WriteLine($"Value \t Count");
+                        foreach (var model in results)
+                        {
+                            Console.WriteLine($"{model.Key} \t {model.Value}");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Error: {ex.Message}");
+                    }
+
                 }
                 else
                 {
-                    Console.WriteLine($"Error: Could not find file at {path}, using default text");
+                    Console.WriteLine($"Error: Could not find file at {path}");
                 }
+
             }
 
-            if (stringToTest.Length < 1)
-            {
-                stringToTest = "Four score and seven years ago our fathers brought forth on this continent, a new nation, conceived in Liberty, and dedicated to the proposition that all men are created equal.";
-            }
-            
-            var service = new HistogramService();
-            var results = service.BuildModel(stringToTest);
+            stopWatch.Stop();
+            var ts = stopWatch.Elapsed;
 
-            Console.WriteLine($"Value \t Count");
-            foreach (var model in results)
-            {
-                Console.WriteLine($"{model.Value} \t {model.Occurences}");
-            }
+            string elapsedTime = $"{ts.Hours:00}:{ts.Minutes:00}:{ts.Seconds:00}.{ts.Milliseconds / 10:00}";
+            Console.WriteLine("RunTime " + elapsedTime);
+
             Console.ReadLine();
         }
     }
 
-    public class HistogramViewModel
-    {
-        public string Value { get; set; }
-        public int Occurences { get; set; }
-    }
 
     public class HistogramService
     {
-        public List<HistogramViewModel> BuildModel(string input)
+        public Dictionary<string, int> ProcessFile(string path)
         {
-            var results = new List<HistogramViewModel>();
+            var dic = new Dictionary<string, int>();
 
-            var source = RemovePunctuation(input);
-            var words = source.ToLower().Split(' ');
 
-            for (var i = 0; i < words.Length - 1; i++)
+            if (File.Exists(path))
             {
-                var phrase = $"{words[i]} {words[i + 1]}";
-                var existing = results.FirstOrDefault(r => r.Value == phrase);
-                if (existing != null)
+
+                try
                 {
-                    existing.Occurences += 1;
-                }
-                else
-                {
-                    results.Add(new HistogramViewModel
+                    using (var fs = File.Open(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                    using (var bs = new BufferedStream(fs))
+                    using (var sr = new StreamReader(bs))
                     {
-                        Value = phrase,
-                        Occurences = 1
-                    });
+                        string line;
+                        while ((line = sr.ReadLine()) != null)
+                        {
+                            var source = RemovePunctuation(line);
+                            var words = source.ToLower().Split(' ');
+
+                            for (var i = 0; i < words.Length - 1; i++)
+                            {
+                                var phrase = $"{words[i]} {words[i + 1]}";
+
+                                var ext = dic.FirstOrDefault(r => r.Key == phrase);
+
+                                if (ext.Value > 0)
+                                {
+                                    dic[phrase] = ext.Value + 1;
+                                }
+                                else
+                                {
+                                    dic.Add(phrase, 1);
+                                }
+
+                            }
+                        }
+                    }
                 }
+                catch (Exception)
+                {
+                    throw;
+                }
+
+
             }
 
-            return results;
-            //return results.OrderByDescending(item => item.Occurences).ThenBy(item => item.Value).ToList();
+            return dic;
         }
+
 
         private static string RemovePunctuation(string input)
         {
-            return input.Replace(".", "").Replace("!", "").Replace("?", "").Replace("\"", "").Replace(",", "");
+            return input.Replace(".", "").Replace("!", "").Replace("?", "").Replace("\"", "").Replace(",", "").Replace("#", "").Replace(":", "").Replace(";", "");
         }
     }
 }
